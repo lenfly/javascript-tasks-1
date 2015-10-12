2efbffa40c5a0117000158ce176dff705260942f
lenfly <shmarova.ea@gmail.com>
2015-10-13 01:12:21 +0500

    Мои исправления_алгоритм,кодстайл

--- roman-time.js
+++ roman-time.js
@@ -1,32 +1,35 @@
+'use strict';
 var hours = process.argv[2];
 var minutes = process.argv[3];
 // Немного замечательного кода и магии
-var font_ar = [1,4,5,9,10,40,50,90,100,400,500,900,1000,4000,5000,9000,10000];
-var font_rom = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M",
-"Mↁ","ↁ","ↁↂ","ↂ"];
-
-function toRoman(text) {
-if (!text) return '';
-var rezult = ''; 
-var n = font_ar.length - 1; 
-while (text > 0) { 
-if (text >= font_ar[n]) { 
-rezult += font_rom[n]; 
-text -= font_ar[n]; 
-}
-else n--; 
-}
-return rezult; 
+var fontAr = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+var fontRom = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+    
+function getRoman(time) {
+    if (!time) return '';
+    var rezult = '';
+    var n = fontAr.length - 1;
+    while (time > 0) {
+        if (time >= fontAr[n]) {
+            rezult += fontRom[n];
+            time -= fontAr[n];
+        } else n--;
+    }
+    return rezult;
 }
 
-function getTrueTime () {
-if ((0 <= hours) && (hours <= 23) && (0 <= minutes) && (minutes <= 59)) {
-console.log('Час: ', toRoman(hours), 'Минута: ', toRoman(minutes));
-} else { 
-console.log('Время указано неверно');
-}
+function getTrueTime() {
+    if (1 <= hours && hours <= 23 && 1 <= minutes && minutes <= 59) {
+        console.log('Час: ', getRoman(hours), 'Минута: ', getRoman(minutes));
+    } else if ((parseInt(hours, 10) === 0 || parseInt(hours, 10) === 24) && (1 <= minutes && minutes <= 59)) {
+        console.log('Час: --', 'Минута: ', getRoman(minutes));
+    } else if (1 <= hours && hours <= 23 && parseInt(minutes, 10) === 0) {
+        console.log('Час: ', getRoman(hours), 'Минута: --');
+    } else if ((parseInt(hours, 10) === 0 || parseInt(hours, 10) === 24) && (parseInt(minutes, 10) === 0)) {
+        console.log('Час: --', 'Минута: --');
+    } else {
+        console.log('Время указано неверно');
+    }
 }
 
 getTrueTime();
-
-
